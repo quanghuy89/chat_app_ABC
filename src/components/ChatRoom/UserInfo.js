24fda7bb876e4d84ffc9ec9aa3446445729b81dc
letiendat1002 <letiendat1002@gmail.com>
2022-06-03 15:08:25 +0700

    format Login UI

--- src/components/ChatRoom/UserInfo.js
+++ src/components/ChatRoom/UserInfo.js
@@ -1,52 +1,53 @@
-import React from 'react';
-import { Button, Avatar, Typography } from 'antd';
-import styled from 'styled-components';
+import React from "react";
+import { Button, Avatar, Typography } from "antd";
+import styled from "styled-components";
 
-import { auth } from '../../firebase/config';
-import { AuthContext } from '../../Context/AuthProvider';
-import { AppContext } from '../../Context/AppProvider';
+import { auth } from "../../firebase/config";
+import { AuthContext } from "../../Context/AuthProvider";
+import { AppContext } from "../../Context/AppProvider";
 
 const WrapperStyled = styled.div`
-  display: flex;
-  justify-content: space-between;
-  padding: 12px 16px;
-  border-bottom: 1px solid rgba(82, 38, 83);
+	display: flex;
+	justify-content: space-between;
+	padding: 12px 16px;
+	border-bottom: 1px solid rgba(82, 38, 83);
 
-  .username {
-    color: #fff;
-    font-size:18px;
-    margin-left: 10px;
-    padding:10px 0px;
-  }
+	.username {
+		color: #fff;
+		font-size: 18px;
+		margin-left: 10px;
+		padding: 10px 0px;
+	}
 `;
 
 // export default function UserInfo() {
 
 // }
 
-
 export default function UserInfo() {
-  const {
-    user: { displayName, photoURL },
-  } = React.useContext(AuthContext);
-  // console.warn('')
-  const { clearState } = React.useContext(AppContext);
+	const {
+		user: { displayName, photoURL },
+	} = React.useContext(AuthContext);
+	const { clearState } = React.useContext(AppContext);
 
-  return (
-    <WrapperStyled>
-      <div>
-        <Avatar src={photoURL}>{photoURL ? '' : displayName?.charAt(0)?.toUpperCase()}</Avatar>
-        <Typography.Text className='username'>{displayName}</Typography.Text>
-      </div>
-      <Button
-        ghost
-        onClick={() => {
-          // clear state in App Provider when logout
-          clearState();
-          auth.signOut();
-        }}>
-        Đăng xuất
-      </Button>
-    </WrapperStyled>
-  );
+	return (
+		<WrapperStyled>
+			<div>
+				<Avatar src={photoURL}>
+					{photoURL ? "" : displayName?.charAt(0)?.toUpperCase()}
+				</Avatar>
+				<Typography.Text className="username">{displayName}</Typography.Text>
+			</div>
+			<Button
+				ghost
+				onClick={() => {
+					// clear state in App Provider when logout
+					clearState();
+					auth.signOut();
+				}}
+			>
+				Đăng xuất
+			</Button>
+		</WrapperStyled>
+	);
 }
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,17 +1,17 @@
-import firebase from 'firebase/app';
+import firebase from "firebase/app";
 
-import 'firebase/analytics';
-import 'firebase/auth';
-import 'firebase/firestore';
+import "firebase/analytics";
+import "firebase/auth";
+import "firebase/firestore";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyDJfTtDCI4XOGXaGLwxHxBaye4WcJI3Ywg",
-  authDomain: "chat-app-13a97.firebaseapp.com",
-  projectId: "chat-app-13a97",
-  storageBucket: "chat-app-13a97.appspot.com",
-  messagingSenderId: "1092770954741",
-  appId: "1:1092770954741:web:21f035fb54b4e969ea74bc",
-  measurementId: "G-WN6RD6FNZ8"
+	apiKey: "AIzaSyD-3w90nWja488dX7oiBVd4yQNm-PZ2R8I",
+	authDomain: "group04-chat-app-407a4.firebaseapp.com",
+	projectId: "group04-chat-app-407a4",
+	storageBucket: "group04-chat-app-407a4.appspot.com",
+	messagingSenderId: "792168196249",
+	appId: "1:792168196249:web:2021f830988f204ec33008",
+	measurementId: "G-JYGZTDC8X3",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -20,9 +20,9 @@ firebase.analytics();
 const auth = firebase.auth();
 const db = firebase.firestore();
 
-if (window.location.hostname === 'localhost') {
-  auth.useEmulator('http://localhost:9099');
-  db.useEmulator('localhost', '8080');
+if (window.location.hostname === "localhost") {
+	auth.useEmulator("http://localhost:9099");
+	db.useEmulator("localhost", "8080");
 }
 
 export { db, auth };
